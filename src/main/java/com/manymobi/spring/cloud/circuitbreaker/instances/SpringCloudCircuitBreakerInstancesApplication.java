b651831051505eed69c1daecec06da5855a88a67
梁建军 <631583871@qq.com>
2020-04-03 14:59:32 +0800

    初始化

---
+++ src/main/java/com/manymobi/spring/cloud/circuitbreaker/instances/SpringCloudCircuitBreakerInstancesApplication.java
@@ -0,0 +1,13 @@
+package com.manymobi.spring.cloud.circuitbreaker.instances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCircuitBreakerInstancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCircuitBreakerInstancesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manymobi/spring/cloud/circuitbreaker/instances/SpringCloudCircuitBreakerInstancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manymobi.spring.cloud.circuitbreaker.instances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCircuitBreakerInstancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
